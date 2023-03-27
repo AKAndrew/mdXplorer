57017e0050febfcd006c4f35e327258f42d39657
AKAndrew <rzm.andrew@gmail.com>
2023-03-27 21:24:54 +0100

    website front-end 100%

--- web-design/theme/mail_handler.php
+++ web-design/theme/mail_handler.php
@@ -1,18 +1,20 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "at1288@live.mdx.ac.uk"; // this is your Email address
+	ini_set( 'display_errors', 1 );
+	error_reporting( E_ALL );
+    $to = "mdxplorer@gymvibeiasi.ro"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = "mdXplorer contact form submission from ".$_POST["name"];
     $subject2 = "Copy of your mdXplorer contact form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+	$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+	if(mail($to,$subject,$message,$headers) && mail($from,$subject2,$message2,$headers2))
+		header('Location: contact.html#success');
+	else
+		header('Location: contact.html#failed');
+}
 ?>
